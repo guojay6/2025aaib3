b6fefd7d5a02531a6ee1024a366434bdfa0027c9
guojay6 <zhongjieguo777@gmail.com>
2025-09-17 11:36:24 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,16 @@
+//week02-1.cpp
+//c
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	int b =a;//backup
+	// Bo-pi Fa
+	int ans = 0;
+	while(a>0){
+		ans = ans*10 + a%10;
+		a = a/=10;
+	}
+	printf("%d+%d=%d\n",b, ans, b+ans);
+}
